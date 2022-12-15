4ba0cb29d2f2aa6caf6c848540faef5cd927a86f
MinhyukWoo <minhyukwoo.dev@gmail.com>
2022-12-15 14:32:43 +0900

    Change message form foucs border

--- chatting_diary/components/ChattingForm.jsx
+++ chatting_diary/components/ChattingForm.jsx
@@ -8,7 +8,7 @@ import {
   TextArea,
   View,
 } from "native-base";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const ChattingForm = (props) => {
   const [message, setMessage] = useState("");
@@ -19,18 +19,22 @@ const ChattingForm = (props) => {
     props.onSend(message);
     setMessage("");
   };
-
   return (
     <Flex direction="row" justify="space-between" align="stretch" m="1">
       <TextArea
         size="md"
         placeholder="채팅하세요."
         bgColor="amber.50"
+        _focus={
+          {borderColor: "amber.200"}
+        }
         flex={1}
         value={message}
         onChangeText={onChangeTextInput}
       ></TextArea>
-      <Button bgColor="amber.500" onPress={onClickSendButton}>전송</Button>
+      <Button bgColor="amber.500" onPress={onClickSendButton}>
+        전송
+      </Button>
     </Flex>
   );
 };
